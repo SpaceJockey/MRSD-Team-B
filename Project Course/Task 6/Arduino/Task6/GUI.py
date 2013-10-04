1f11361595c84d927eaea54fcab4921f3ef079fb
boyleb2 <boyleb2@andrew.cmu.edu>
2013-10-04 11:04:14 -0400

    initial commit of gui skeleton
    make sure you have python 2.7 installed plus the tkinter and serial
    libraries

---
+++ Project Course/Task 6/Arduino/Task6/GUI.py
@@ -0,0 +1,90 @@
+from Tkinter import *
+from serial import *
+import time
+
+port = "COM3"
+baudRate= 9600
+#ser = Serial(port, baudRate, timeout=0,writeTimeout=0)
+#print "Opening Serial Port..."
+#time.sleep(1)
+
+thermalVal, forceVal, lightVal, rangeVal = (1.0,)*4
+
+root = Tk()
+
+def ping():
+    print "Ping!"
+    ser.write("ping\n")
+
+def setServo():
+    print servo_val.get()
+	
+def updateSensors():
+    global thermalVal, forceVal, lightVal, rangeVal
+    print "Updating..."
+    
+    thermalVal = thermalVal + 1 * 0.9
+    forceVal = forceVal *1.01
+    lightVal = lightVal *1.1
+    rangeVal = rangeVal + 0.1
+    
+    thermalVar.set("%4.3f" % thermalVal)
+    forceVar.set("%4.3f" % forceVal)
+    lightVar.set("%4.3f" % lightVal)
+    rangeVar.set("%4.3f" % rangeVal)
+
+    root.after(1000,updateSensors)
+	
+def moveStepper():
+    print stepper_val.get()
+	
+	
+root.wm_title("MRSD Sensors and Motors Lab")
+root.config(background="#DDDDDD")
+
+# Left frame - Sensor Outputs
+
+leftFrame = Frame(root, width=300,height=400)
+leftFrame.grid(row=0, column=0, padx=10, pady=2)
+
+Label(leftFrame, text="Sensor Outputs").grid(row=0, column=0, columnspan=2,padx=2, pady=2)
+Label(leftFrame, text="Thermal:").grid(row=1,column=0,padx=2,pady=1, sticky=E)
+Label(leftFrame, text="Force:").grid(row=2,column=0,padx=2,pady=1, sticky=E)
+Label(leftFrame, text="Light:").grid(row=3,column=0,padx=2,pady=1, sticky=E)
+Label(leftFrame, text="Range:").grid(row=4,column=0,padx=2,pady=1, sticky=E)
+
+thermalVar = StringVar()
+forceVar = StringVar()
+lightVar = StringVar()
+rangeVar = StringVar()
+updateSensors()
+
+Label(leftFrame, textvariable=thermalVar).grid(row=1,column=1,pady=1,stick=W)
+Label(leftFrame, textvariable=forceVar).grid(row=2,column=1,pady=1,stick=W)
+Label(leftFrame, textvariable=lightVar).grid(row=3,column=1,pady=1,stick=W)
+Label(leftFrame, textvariable=rangeVar).grid(row=4,column=1,pady=1,stick=W)
+
+
+# Right Frame - Motor Controls	
+
+rightFrame = Frame(root, width=200, height = 400)
+rightFrame.grid(row=0, column=1, padx=10, pady=2)
+
+Label(rightFrame, text="Motor Controls").grid(row=0, column=0, columnspan=3, padx=10, pady=2)
+Label(rightFrame, text="Servo:").grid(row=1,column=0,padx=2,pady=1, sticky=E)
+Label(rightFrame, text="DC:").grid(row=2,column=0,padx=2,pady=1, sticky=E)
+Label(rightFrame, text="Stepper:",).grid(row=3,column=0,padx=2,pady=1, sticky=E)
+
+servo_val = StringVar()
+servo_entry = Entry(rightFrame,textvariable=servo_val,width=4).grid(row=1,column=1)
+servo_set = Button(rightFrame,text="Set",command=setServo).grid(row=1,column=2,padx=5)
+
+stepper_val = StringVar()
+stepper_entry = Entry(rightFrame,textvariable=servo_val,width=4).grid(row=3,column=1)
+stepper_set = Button(rightFrame,text="Move",command=moveStepper).grid(row=3,column=2,padx=5)
+
+
+#pingButt = Button(rightFrame, text="Ping", command=ping)
+#pingButt.grid(row=1,column=1,padx=10,pady=2)
+
+root.mainloop()
