39f36df9a5e6b488674748fdd7d15a63c5220567
Nathaniel Chapman <nate@nathanielchapman.com>
2013-11-23 17:49:27 -0500

    Servo Calibration Done, Draft ROSListener written
    Just waiting for Brian to figure our ROSSerial...

--- Project/Arduino/ROSListener/ROSListener.ino
+++ Project/Arduino/ROSListener/ROSListener.ino
@@ -1,4 +1,19 @@
+/*
+ROSListener.ino
+Written by Nathaniel Chapman, 11/23/2013
+MRSD Team B
+This Sketch listens to the ROS channel "serial_link" and updates joint positions accordingly
 
+ROS Joint configs are indexed so...
+0	Center Swivel
+1	Center Segment Front Pitch
+2	Center Segment Rear Pitch
+3	Front Segment Pitch
+4	Rear Segment Pitch
+5	Center Segment Front Prismatic
+6	Center Segment Rear Prismatic
+
+*/
 
 #include <ros.h>
 #include <sensor_msgs/JointState.h>
@@ -11,153 +26,82 @@
 
 // Ros Initialization stuff
 ros::NodeHandle  nh;
-sensor_msgs::JointState pub_msg; //out
 
-//ros::Publisher left_encoder_pub("left_encoder", &l_enc_msg);
-//ros::Publisher right_encoder_pub("right_encoder", &r_enc_msg);
-
-int blink = 1;
 //Position Update Callback
+int blink = 1;
 void jointstate_cb(const sensor_msgs::JointState& cmd_msg){
+  //blink LED to indicate CB is running...
   blink = 1-blink;
   digitalWrite(STATUS_LED, blink);
-  //servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
-  //digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+  
+  for(int c = 0; c < cmd_msg.position_length; c++) setServoPos(c, cmd_msg.position[c]); //Update servos
 }
-
-
-ros::Subscriber<sensor_msgs::JointState> sub("serial_link", jointstate_cb);
-
+ros::Subscriber<sensor_msgs::JointState> serial_link("serial_link", jointstate_cb);
 
 Adafruit_PWMServoDriver hv_servo = Adafruit_PWMServoDriver();
-
-//for convenience, let's overload map to accept float params for our radian values from ROS
-long map(float x, float in_min, float in_max, long out_min, long out_max)
-{
-  return ((long) (x - in_min) * ((float) (out_max - out_min)) / (in_max - in_min)) + out_min;
-}
+//Adafruit_PWMServoDriver hv_servo = Adafruit_PWMServoDriver(0x41);
 
 //Radian values for us degree-centric folks
 #define M_PI 3.14159265
 #define DEG_20 (M_PI / 9)
 #define DEG_30 (M_PI / 6)
-
-float degtorad(float deg){
-  return (deg * M_PI) / 180.0;
-}
-
-/* joints are as follows:
-0	Center Swivel
-1	Center Segment Front Pitch
-2	Center Segment Rear Pitch
-3	Front Segment Pitch
-4	Rear Segment Pitch
-5	Center Segment Front Prismatic
-6	Center Segment Rear Prismatic
-*/
+#define DEG_45 (M_PI / 4)
+//float degtorad(float deg){return (deg * M_PI) / 180.0;}
 
 #define DIG_SERVO_HZ  300 // Digital Servos operate at 300 Hz
 #define ANA_SERVO_HZ  50 // Analog Servos operate at 50 Hz
 
-#define SERVOMIN  170 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMID  335 // This is the Servo 'Middle Position'
-#define SERVOMAX  500 // this is the 'maximum' pulse length count (out of 4096)
-
-//Linear servo calibration defaults
-#define LIN_SOFT_MIN 350
-#define LIN_RANGE 190
+#define SERVOMIN  185 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMID  307 // This is the Servo 'Middle Position'
+#define SERVOMAX  430 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVORANGE ((float) (SERVOMAX - SERVOMIN))
 
 //These are the configuration values for the robot, they need to be tuned!
 //These are in radians or meters, depending on joint types
-const float real_min[] = { DEG_20,  DEG_20,  DEG_20,  DEG_30,  DEG_20,   0,   0};
-const float real_max[] = {-DEG_20, -DEG_20, -DEG_20, -DEG_30, -DEG_20, .145, .145};
+const float real_min[] = {-DEG_30, -DEG_30, -DEG_30, -DEG_45, -DEG_45,   0,   0};
+const float real_max[] = { DEG_30,  DEG_30,  DEG_30,  DEG_45,  DEG_45, .145, .145};
 
-//Linear values are not configured yet...
-long servo_min[] = {SERVOMIN, SERVOMIN, SERVOMIN, SERVOMAX, SERVOMAX - 100, LIN_SOFT_MIN + 100, LIN_SOFT_MIN + 100};
-long servo_max[] = {SERVOMAX, SERVOMAX, SERVOMAX, SERVOMIN, SERVOMIN + 100, LIN_SOFT_MIN, LIN_SOFT_MIN};
-
-const unsigned int servo_addr[] = {12, 4, 8, 5, 9, 0, 1};
+//Servo addresses for the HV servo board
+const unsigned int hv_servo_addr[] = {12, 4, 8, 5, 9, 0, 1};
 
 //used only for linear joints where they have a limit switch
 const unsigned int limit_pin[] = {0, 0, 0, 0, 0, 52, 53};
 
-void set_rot(unsigned int joint, float radians){
- if(joint > 4) return; //only work on the rotary joints...
- 
- int sval = map(radians, real_min[joint], real_max[joint], servo_min[joint], servo_max[joint]);
- hv_servo.setPWM(servo_addr[joint], 0, sval);
+//To map things to our servo values
+int servoMap(float value, unsigned int joint)
+{return ((int) (((value - real_min[joint]) * SERVORANGE)  / (real_max[joint] - real_min[joint]))) + SERVOMIN;}
+
+//Set the specified servo to the correct real-world position
+void setServoPos(unsigned int joint, float value){
+  if(joint > 6) return; //ignore unimplemented channels
+  unsigned int sval = servoMap(value, joint);
+  hv_servo.setPWM(hv_servo_addr[joint], 0, servoMap(value, joint));
 }
-
-/*
-
-
-void calibrate_linear(unsigned int joint){
- if(joint < 5 || joint > 6) return; //only work on the linear joints...
-
- int servoval = servo_min[joint];
-  //Serial.print("Linear joint ");
-  //Serial.print(joint);
-  //Serial.println(" Calibrating...");
-	
-  hv_servo.setPWM(servo_addr[joint], 0, servoval);
-  delay(3000);
-  
-  for (; digitalRead(limit_pin[joint]); servoval++){
-    hv_servo.setPWM(servo_addr[joint], 0, servoval);
-	delay(50);
-  }
-	//Serial.print("Minimum: ");
-	servo_min[joint] = servoval;
-	//Serial.println(servo_min[joint]);
-	//Serial.print("Est. Maximum: ");
-	servo_max[joint] = servoval - LIN_RANGE;
-	//Serial.println(servo_max[joint]);
-	
-	hv_servo.setPWM(servo_addr[joint], 0, servo_max[joint]);
-}
-*/
 
 void setup() {
-  //Serial.begin(9600);
-  //Serial.println("Space Jockey Robot Actuator Calibration!");
-  
+  //Set up status LED
   pinMode(STATUS_LED, OUTPUT);
   digitalWrite(STATUS_LED, LOW);
   
-  //Set up ROS node
-  nh.getHardware()->setBaud(115200); // Up Baud Rate
-  nh.initNode();
-  nh.subscribe(sub);
-
-  //Set up limit switches
+  //Set up limit switch pins
   pinMode(limit_pin[5], INPUT);
   digitalWrite(limit_pin[5], HIGH); 
   pinMode(limit_pin[6], INPUT);
   digitalWrite(limit_pin[6], HIGH);
   
+  //Set up ROS node
+  //nh.getHardware()->setBaud(115200); // Up Baud Rate
+  nh.initNode();
+  nh.subscribe(serial_link);
+  
   //initialize digital servo board
   hv_servo.begin();  
   hv_servo.setPWMFreq(ANA_SERVO_HZ);
-  
-  //Set Rotary joints to middle range
-  /*
-  for(int srv = 2; srv < 16; srv++){ 
-	hv_servo.setPWM(srv, 0, SERVOMID);
-  }
-  */
-  
+    
 }
 
-
 void loop() {
-  set_rot(3, real_max[3]);
-  digitalWrite(STATUS_LED, HIGH);
-  delay(1000);
-  set_rot(3, real_min[3]);
-  digitalWrite(STATUS_LED, LOW);
-  delay(1000);
-  
-  //spin ROS
-  //nh.spinOnce();
-  //delay(1);
+	//spin ROS
+	nh.spinOnce();
+	delay(1);
 }
--- Project/Arduino/RotaryServoCalibrate/RotaryServoCalibrate.ino
+++ Project/Arduino/RotaryServoCalibrate/RotaryServoCalibrate.ino
@@ -1,58 +1,87 @@
-/*************************************************** 
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 16 servos, one after the other
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These displays use I2C to communicate, 2 pins are required to  
-  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+//General Debugging stuff
+#define STATUS_LED 13
 
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
-#define SERVOMIN  250 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  450 // this is the 'maximum' pulse length count (out of 4096)
-#define SERVO_HZ  60 // Digital Servos operate at 300 Hz
+Adafruit_PWMServoDriver hv_servo = Adafruit_PWMServoDriver();
 
-#define ROT_SERVO_1 4
-#define ROT_SERVO_2 5
+//Radian values for us degree-centric folks
+#define M_PI 3.14159265
+#define DEG_20 (M_PI / 9)
+#define DEG_30 (M_PI / 6)
+#define DEG_45 (M_PI / 4)
+
+/* joints are as follows:
+0	Center Swivel
+1	Center Segment Front Pitch
+2	Center Segment Rear Pitch
+3	Front Segment Pitch
+4	Rear Segment Pitch
+5	Center Segment Front Prismatic
+6	Center Segment Rear Prismatic
+*/
+
+#define DIG_SERVO_HZ  300 // Digital Servos operate at 300 Hz
+#define ANA_SERVO_HZ  50 // Analog Servos operate at 50 Hz
+
+
+#define SERVOMIN  185 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMID  307 // This is the Servo 'Middle Position'
+#define SERVOMAX  430 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVORANGE ((float) (SERVOMAX - SERVOMIN))
+
+//These are the configuration values for the robot, they need to be tuned!
+//These are in radians or meters, depending on joint types
+const float real_min[] = {-DEG_30, -DEG_30, -DEG_30, -DEG_45, -DEG_45,   0,   0};
+const float real_max[] = { DEG_30,  DEG_30,  DEG_30,  DEG_45,  DEG_45, .145, .145};
+
+//Servo addresses for the HV servo board
+const unsigned int hv_servo_addr[] = {12, 4, 8, 5, 9, 0, 1};
+
+//used only for linear joints where they have a limit switch
+const unsigned int limit_pin[] = {0, 0, 0, 0, 0, 52, 53};
+
+//To map things to our servo values
+int servoMap(float value, unsigned int joint)
+{return ((int) (((value - real_min[joint]) * SERVORANGE)  / (real_max[joint] - real_min[joint]))) + SERVOMIN;}
+
+//Set the specified servo to the correct real-world position
+void setServoPos(unsigned int joint, float value){
+  unsigned int sval = servoMap(value, joint);
+  hv_servo.setPWM(hv_servo_addr[joint], 0, servoMap(value, joint));
+  Serial.print("Setting Channel ");
+  Serial.print(joint);
+  Serial.print(" to servo position: ");
+  Serial.println(sval);
+}
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("2 Channel Rotary Actuator Calibration Test!");
-
-  pwm.begin();  
-  pwm.setPWMFreq(SERVO_HZ);
- 
+  Serial.println("Space Jockey Robot Servo Calibration Test!");
+  
+  pinMode(STATUS_LED, OUTPUT);
+  digitalWrite(STATUS_LED, LOW);
+  
+  //initialize digital servo board
+  hv_servo.begin();  
+  hv_servo.setPWMFreq(50);
+    
 }
 
 
 void loop() {
-  Serial.println("Left!");
-  pwm.setPWM(ROT_SERVO_1, 0, SERVOMAX);
-  pwm.setPWM(ROT_SERVO_2, 0, SERVOMIN);
-  delay(1000);
-  
-  Serial.println("Right!");
-  pwm.setPWM(ROT_SERVO_1, 0, SERVOMIN);
-  pwm.setPWM(ROT_SERVO_2, 0, SERVOMAX);
-  delay(1000);
+	int c = 3;
+	for (float f = 0.0; f < 1.00; f = f + .005){
+		Serial.print("Ratio: ");
+		Serial.println(f);
+		float real_val = real_min[c] + (real_max[c] - real_min[c]) * f;
+		Serial.print("Setting Channel ");
+		Serial.print(c);
+		Serial.print(" to real position: ");
+		Serial.println(real_val);
+		setServoPos(c, real_val);
+		delay(10);
+	}
 }
