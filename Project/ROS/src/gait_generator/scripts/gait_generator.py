1ff6315c47071125e489770f1b4645334cbb1104
rusty <rusty@rusty-VirtualBox.(none)>
2013-11-23 12:06:07 -0500

    added ROS src code

---
+++ Project/ROS/src/gait_generator/scripts/gait_generator.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+import rospy, time, roslib, os, rospkg
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+from rqt_sj.msg import GaitComm
+from math import pi, radians, degrees, cos, sin, tan, sqrt
+
+
+CENTER_SWIVEL = 0
+CENTER_PITCHF = 1
+CENTER_PITCHR = 2
+FRONT_PITCH = 3
+REAR_PITCH = 4
+CENTER_PRISMF = 5
+CENTER_PRISMR = 6
+
+def get_prism_ang(dist, clear):
+  prism = sqrt(dist*dist + clear*clear)
+  ang = tan(clear/dist)
+  return prism, ang
+
+class GaitGenerator():
+    prism_len = 0.15
+    clearance = 0.01
+    lift_res = 20
+    xtend_res = 40
+    max_xtend = 0.10
+    x_dist = sqrt((max_xtend + prism_len)*(max_xtend + prism_len) - clearance*clearance)
+    
+    dsb_joints = [0.0,]*12
+    joint_msg = JointState()
+    joint_msg.name = ['center_swivel','fore_base_pitch','aft_base_pitch','front_pitch','rear_pitch','fore_extend','aft_extend']
+    joint_msg.position = dsb_joints[0:7]
+    joint_msg.velocity=[0.0,]*7
+    
+    
+    
+    schedPub = rospy.Publisher('sched_joints',JointState)
+    jointPub = rospy.Publisher('joint_states',JointState)
+    
+    def __init__(self):
+      self.listener()
+      print("Gait Generator online.")
+      rospy.spin()
+      
+    def command_receive(self,msg):
+      print(msg.data)
+      data = msg.data.split()
+      if data[0] == "clearance":
+        self.clearance = float(data[1])
+        self.x_dist = sqrt((self.max_xtend + self.prism_len)*(self.max_xtend + self.prism_len) - self.clearance*self.clearance)
+      elif data[0] == "liftres":
+        self.lift_res = int(data[1])
+      elif data[0] == "extendres":
+        self.xtend_res = int(data[1])
+      elif data[0] == "maxextend":
+        self.max_xtend = float(data[1])
+        self.x_dist = sqrt((self.max_xtend + self.prism_len)*(self.max_xtend + self.prism_len) - self.clearance*self.clearance)
+      else:
+        if data[1] == 'front' or data[1] == 'rear':
+          if data[1] == 'front':
+            center_pitch = CENTER_PITCHF
+            end_pitch = FRONT_PITCH
+            prism = CENTER_PRISMF
+          elif data[1] == 'rear':
+            center_pitch = CENTER_PITCHR
+            end_pitch = REAR_PITCH
+            prism = CENTER_PRISMR    
+                      
+          if data[0] == 'lift' or data[0] == 'place':
+            lift = data[0] == 'lift'
+            extended = data[2] == 'extended'
+            self.execute_lift_place(center_pitch,end_pitch,prism, extended, lift)
+          elif data[0] == 'extend' or data[0] == 'retract':
+            extend = data[0] == 'extend'
+            self.execute_extend_retract(center_pitch,end_pitch,prism, extend)
+            
+        elif data[1] == 'middle':
+          if data[0] == 'lift' or data[0] == 'place':
+            self.execute_middle_lift_place(data[2], data[0]=='lift')        
+
+
+    
+    def listener(self):
+      rospy.init_node('gait_generator', anonymous=True)
+      rospy.Subscriber("gait_commands", String, self.command_receive)
+
+    def updateJointState(self):
+        time.sleep(0.01)
+        self.joint_msg.header.stamp = rospy.Time.now()
+        self.joint_msg.position = self.dsb_joints[0:7]
+        self.schedPub.publish(self.joint_msg)
+
+    # Executes a lift or place operation
+    # center_pitch: the slider widget for the appropriate center segment pitch joint
+    # end_pitch: '' end segment pitch joint
+    # center_prism: '' prismatic joint
+    # lift: boolean, TRUE for lift, FALSE for place
+    # extended: boolean, TRUE if extended, FALSE if retracted
+    # IMPORTANT: To PLACE, rather than lift, reverse center_pitch and end_pitch
+    def execute_lift_place(self, center_pitch, end_pitch, center_prism, extended, lift):
+        for t in range(1,self.lift_res+1):
+          if lift:
+            c = self.clearance*t/self.lift_res
+          else:
+            c = self.clearance*(self.lift_res-t)/self.lift_res
+          if extended:
+            dist = self.x_dist
+          else:
+            dist = self.prism_len
+          prism,ang = get_prism_ang(dist, c)
+          print(dist)
+          self.dsb_joints[center_pitch] = ang
+          self.dsb_joints[end_pitch] = -ang
+          self.dsb_joints[center_prism] = (prism-self.prism_len)
+
+          self.updateJointState() 
+      
+      
+    # Executes an extend or retract operation
+    # center_pitch: the slider widget for the appropriate center segment pitch joint
+    # end_pitch: '' end segment pitch joint
+    # center_prism: '' prismatic joint
+    # extend: boolean, TRUE for extending, FALSE for retracting
+    def execute_extend_retract(self, center_pitch, end_pitch, center_prism, extend):
+        for t in range(1,self.xtend_res+1):
+          if extend:
+            nlen = self.prism_len + (self.x_dist-self.prism_len)*t/self.xtend_res
+          else:
+            nlen = self.prism_len + (self.x_dist-self.prism_len)*(self.xtend_res - t)/self.xtend_res
+            
+          prism, ang = get_prism_ang(nlen,self.clearance)
+          self.dsb_joints[center_pitch] = ang
+          self.dsb_joints[end_pitch] = -ang
+          self.dsb_joints[center_prism] = (prism-self.prism_len)
+          
+          self.updateJointState()      
+      
+      
+    # Executes a lift of the middle section
+    # facing: 'fixed', 'forward', or 'backward'
+    #   fixed: both ends retracted
+    #   forward: front end extended
+    #   backward: rear end extended
+    # lift: lifting or placing, lift = True, place = False
+    def execute_middle_lift_place(self, facing, lift):
+        if facing == 'closed':
+          xlen = self.prism_len
+          xlen2 = self.prism_len
+        elif facing == 'open':
+          xlen = self.x_dist
+          xlen2 = self.x_dist
+        elif facing == 'forward':
+          xlen = self.x_dist
+          xlen2 = self.prism_len
+        elif facing == 'backward':
+          xlen = self.prism_len
+          xlen2 = self.x_dist
+        for t in range(1,self.lift_res+1):
+        
+          c = (self.clearance*t/self.lift_res) if lift else (self.clearance*(self.lift_res-t)/self.lift_res)
+
+          prism, ang = get_prism_ang(xlen,c)          
+          self.dsb_joints[CENTER_PITCHF] = -ang
+          self.dsb_joints[FRONT_PITCH] = ang
+          self.dsb_joints[CENTER_PRISMF] = prism - self.prism_len
+          
+          prism, ang = get_prism_ang(xlen2,c)
+          self.dsb_joints[CENTER_PITCHR] = -ang
+          self.dsb_joints[REAR_PITCH] = ang
+          self.dsb_joints[CENTER_PRISMR] = prism - self.prism_len             
+          
+          self.updateJointState()  
+      
+      
+      
+    def execute_middle_extend_retract(self):
+        if facing == 'forward':
+          xlen = self.x_dist
+          xlen2 = self.prism_len
+        elif facing == 'backward':
+          xlen = self.prism_len
+          xlen2 = self.x_dist
+        for t in range(1, self.xtend_res+1):
+          ang = 0
+          
+          prism, ang = get_prism_ang(xlen,c)          
+          self.dsb_joints[CENTER_PITCHF] = -ang
+          self.dsb_joints[FRONT_PITCH] = ang
+          self.dsb_joints[CENTER_PRISMF] = prism - self.prism_len
+          
+          prism, ang = get_prism_ang(xlen2,c)
+          self.dsb_joints[CENTER_PITCHR] = -ang
+          self.dsb_joints[REAR_PITCH] = ang
+          self.dsb_joints[CENTER_PRISMR] = prism - self.prism_len   
+            
+        ext = 0
+        xlen = 0.15
+        xlen2 = 0.3
+        clear = 0.05
+        dist = sqrt(xlen*xlen - clear*clear)
+        for t in range(0,50):
+          nlen = xlen + dist*(t/50.0)
+          prism, ang = get_prism_ang(nlen,clear)
+          self.center_pitchr_sld.setValue(-ang)
+          self.rear_pitch_sld.setValue(ang)
+          self.center_prismr_sld.setValue((prism-xlen)*1000)
+          
+          nlen = xlen + dist*((50-t)/50.0)
+          prism, ang = get_prism_ang(nlen,clear)
+          self.center_pitchf_sld.setValue(-ang)
+          self.front_pitch_sld.setValue(ang)
+          self.center_prismf_sld.setValue((prism-xlen)*1000)
+          
+          self.updateJointState()
+          #time.sleep(self.delay)
+      
+      
+print("Gait Generator Initializing...")
+gaitgen = GaitGenerator()
---
+++ Project/ROS/src/scheduler/scripts/scheduler.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import rospy, time
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+
+    
+class Scheduler():
+    delay = 1       # Default delay between queue pops
+    pause = False   # Not-paused by default
+    queue = []      # Joint configuration queue
+    
+    # Pubisher for joint states
+    jointPub = rospy.Publisher('joint_states',JointState)
+    
+    # Init: Set up listener
+    def __init__(self):
+      rospy.init_node('scheduler', anonymous=True)
+      self.listener()
+      print("Scheduler online.")
+      
+    # Listens for commands and joint configurations to add to the queue
+    def listener(self):
+      rospy.Subscriber("sched_joints", JointState, self.addto_scheduler)
+      rospy.Subscriber("sched_commands", String, self.command_scheduler)   
+      
+    # Adds a joint configuration to the queue
+    def addto_scheduler(self,msg):
+      print("Scheduler received a joint state!")
+      self.queue.insert(0,msg)
+      
+    # Receives commands from the main GUI
+    def command_scheduler(self,msg):
+      print("Scheduler: " + msg.data)
+      if(msg.data == "pause"):
+        self.pause = True
+      elif(msg.data == "resume"):
+        self.pause = False
+      elif(msg.data == "clear"):
+        self.queue = []
+      elif(msg.data.split()[0]=="delay"):
+        self.delay = float(msg.data.split()[1])
+ 
+
+print("Starting up scheduler...")
+sched = Scheduler()
+
+# Scheduler loop. While not paused, continually print queue size every (delay) seconds.
+# If queue larger than zero, pop a joint configuration each cycle
+#   and publish it to joint_states to be received by rViz and/or the orbot
+while (True):
+  time.sleep(sched.delay)
+  if (not sched.pause):
+    if (len(sched.queue) > 0):
+      comm = sched.queue.pop()
+      print(str(comm))
+      sched.jointPub.publish(comm)
+    print("Queue Size: " + str(len(sched.queue)))
