2033722eba301953a03ac1a620a672114c229542
ardyadipta <ardyadipta@gmail.com>
2014-03-04 13:46:25 -0500

    Apriltags Code
    Apriltags Code from MIT and modified

---
+++ ROS/src/apriltags/mit code/apriltags/pod-build/example/CMakeFiles/imu.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ardyadipta/apriltags/example/Serial.cpp" "/home/ardyadipta/apriltags/pod-build/example/CMakeFiles/imu.dir/Serial.cpp.o"
+  "/home/ardyadipta/apriltags/example/imu.cpp" "/home/ardyadipta/apriltags/pod-build/example/CMakeFiles/imu.dir/imu.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ardyadipta/apriltags/pod-build/CMakeFiles/apriltags.dir/DependInfo.cmake"
+  )
