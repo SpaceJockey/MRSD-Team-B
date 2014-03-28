4f03c16738432ea0c66ea5c031c50fcf2d5510a0
Unchapped <nate@nathanielchapman.com>
2014-03-28 17:59:38 -0400

    started kinematics submodule

---
+++ ROS/src/spacejockey/src/spacejockey/kinematics.py
@@ -0,0 +1,14 @@
+
+import roslib
+roslib.load_manifest('spacejockey')
+import rospy
+
+#TODO: Parse URDF for joint infos....
+#from urdf_parser_py.urdf import URDF
+
+def IKSolver(checklimits=True):
+	return
+	
+BLAH = 1
+def foobar():
+	return True
