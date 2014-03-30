f6e76719ad2482849f5a6ac491dd9fa97f951f5f
Unchapped <nate@nathanielchapman.com>
2014-03-30 11:29:48 -0400

    added python URDF parser, prototyped kinematics functions

--- ROS/src/spacejockey/src/spacejockey/kinematics.py
+++ ROS/src/spacejockey/src/spacejockey/kinematics.py
@@ -2,13 +2,26 @@
 import roslib
 roslib.load_manifest('spacejockey')
 import rospy
+import tf
+
+#pull in the link specs from our URDF file
+from urdf_parser_py.urdf import URDF
+robot = URDF.from_parameter_server()
+
+def printRobot():
+	print(robot)
+	return
 
 #TODO: Parse URDF for joint infos....
 #from urdf_parser_py.urdf import URDF
-
-def IKSolver(checklimits=True):
-	return
+def FK(joint_pos, checklimits=True):
 	
-BLAH = 1
-def foobar():
-	return True
+	return ((f_trans, f_rot), (c_trans, c_rot), (b_trans, b_rot))
+
+def IK((f_trans, f_rot), (c_trans, c_rot), (b_trans, b_rot), checklimits=True):
+
+	return
+
+"""return the normalized torques on each joint given a joint_position, and unit vector in the center joint frame"""
+def unit_jacobian(joint_pos, vector):
+	return
