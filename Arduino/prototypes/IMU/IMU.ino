4f0d7727612f9250f8833aa504e65d31631b377a
Unchapped <nate@nathanielchapman.com>
2014-04-01 22:06:28 -0400

    got IMU prototype code ready for integration

--- Arduino/prototypes/IMU/IMU.ino
+++ Arduino/prototypes/IMU/IMU.ino
@@ -2,93 +2,33 @@
 // -----------------------------
 
 #include <Arduino.h>
+#include <Scheduler.h>
 #include <Wire.h>
 #include "MPU6050.h"
 
 //ROS stuff
 #include <ros.h>
-#include <std_msgs/Int16MultiArray.h>
-//#include <std_msgs/Float32.h>
 
 //ROS Stuff
 ros::NodeHandle nh; //declared "extern" in SpaceJockey.h
-static std_msgs::Int16MultiArray imu_msg; //imu state output
-static ros::Publisher imu_state("imu_data", &imu_msg);
-
-/*
-should we have separate topics for temperature and such, 
-or just dump everything, and let others cherry-pick upstream
-static std_msgs::Float32 temp_msg; //imu temperature output, in degrees C
-static ros::Publisher temp_state("temperature_data", &temp_msg);
-*/
-
-MPU6050 IMU = MPU6050(&Wire);
 
 void setup()
 { 
   //Set up ROS node
   nh.initNode();
-  nh.advertise(imu_state);
-  //nh.advertise(temp_state);
+
+  IMU.begin();
 
   //spin until rosserial connection is live
   while (!nh.connected()) nh.spinOnce();
   nh.loginfo("IMU Dumper Arduino Connected.");
-
-  IMU.begin();
 }
 
 
 void loop()
 {
-  int error;
-  double dT;
-  accel_t_gyro_union accel_t_gyro;
-
-  //Read IMU data
-  error = IMU.read (MPU6050_ACCEL_XOUT_H, (uint8_t *) &accel_t_gyro, sizeof(accel_t_gyro));
-  //TODO: fix this: if(error != 0) nh.logerror("IMU Read Error: " + itos(error));
-
-  // Swap all high and low bytes to low byte first.
-  uint8_t swap;
-  #define SWAP(x,y) swap = x; x = y; y = swap
-
-  SWAP (accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.x_accel_l);
-  SWAP (accel_t_gyro.reg.y_accel_h, accel_t_gyro.reg.y_accel_l);
-  SWAP (accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
-  SWAP (accel_t_gyro.reg.t_h, accel_t_gyro.reg.t_l);
-  SWAP (accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
-  SWAP (accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
-  SWAP (accel_t_gyro.reg.z_gyro_h, accel_t_gyro.reg.z_gyro_l);
-
-
-  /*Dump the raw IMU data
-  in order to get the value in g unit, it has to be divided by 16384 
-  When the sensor is placed perfectly leveled with the ground,
-  the value of x and y is 0, and the value of z is +16384 at a sensitivity of 2g.  
-  In order to get the real value, we have to multiplied it by 1g/16384 
-  in this case, to keep the value is still readable and still in integer type, 
-  I divided by 164,
-  which means the unit is in g/100
-  */
-  accel_t_gyro.value.x_accel = accel_t_gyro.value.x_accel/164;
-  accel_t_gyro.value.y_accel = accel_t_gyro.value.y_accel/164;
-  accel_t_gyro.value.z_accel = accel_t_gyro.value.z_accel/164;
-  imu_msg.data_length = sizeof(accel_t_gyro);
-  imu_msg.data = (int16_t *) &accel_t_gyro;
-  imu_state.publish(&imu_msg);
-
-
-  // The temperature sensor is -40 to +85 degrees Celsius.
-  // It is a signed integer.
-  // According to the datasheet: 
-  //   340 per degrees Celsius, -512 at 35 degrees.
-  // At 0 degrees: -512 - (340 * 35) = -12412
-  /*
-  temp_msg.data = ((float) accel_t_gyro.value.temperature + 12412.0) / 340.0;
-  temp_state.publish( &temp_msg );
-  */
-  delay(1000);
+  nh.spinOnce();
+  delay(1);
 }
 
 
--- Arduino/prototypes/IMU/MPU6050.h
+++ Arduino/prototypes/IMU/MPU6050.h
@@ -1,12 +1,7 @@
 #ifndef MPU6050_H
 #define MPU6050_H
 
-// Nate removed all the unused Register Defs...
-
 // Default I2C address for the MPU-6050 is 0x68.
-// But only if the AD0 pin is low.
-// Some sensor boards have AD0 high, and the
-// I2C address thus becomes 0x69.
 #define MPU6050_I2C_ADDRESS 0x68
 
 //Sleep Ctl Register
@@ -28,17 +23,23 @@
 #define MPU6050_GYRO_ZOUT_H        0x47   // R  
 #define MPU6050_GYRO_ZOUT_L        0x48   // R  
 
+#include <Arduino.h>
+#include <Scheduler.h>
+#include <Wire.h>
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
+
+extern ros::NodeHandle nh;
+
+static float accel_f[3];
+static std_msgs::Float32MultiArray imu_msg; //imu state output
+static ros::Publisher imu_state("accel_data", &imu_msg);
 
 // Declaring an union for the registers and the axis values.
-// The byte order does not match the byte order of 
-// the compiler and AVR chip.
-// The AVR chip (on the Arduino board) has the Low Byte 
-// at the lower address.
-// But the MPU-6050 has a different order: High Byte at
-// lower address, so that has to be corrected.
-// The register part "reg" is only used internally, 
-// and are swapped in code.
-typedef union accel_t_gyro_union
+// NOTE: The byte order does not match
+// The AVR chip has the Low Byte at the lower address.
+// But the MPU-6050 has the High Byte at the lower address
+typedef union accel_t_union
 {
   struct
   {
@@ -48,127 +49,80 @@ typedef union accel_t_gyro_union
     uint8_t y_accel_l;
     uint8_t z_accel_h;
     uint8_t z_accel_l;
-    uint8_t t_h;
-    uint8_t t_l;
-    uint8_t x_gyro_h;
-    uint8_t x_gyro_l;
-    uint8_t y_gyro_h;
-    uint8_t y_gyro_l;
-    uint8_t z_gyro_h;
-    uint8_t z_gyro_l;
   } reg;
-  struct 
-  {
-    int16_t x_accel;
-    int16_t y_accel;
-    int16_t z_accel;
-    int16_t temperature;
-    int16_t x_gyro;
-    int16_t y_gyro;
-    int16_t z_gyro;
-  } value;
+  int16_t accel[3];
 };
 
+//stub, implementation is after the class
+void imuLoop();
 
 class MPU6050{
 	private:
 		TwoWire *_wire;
+		accel_t_union _accel_t;
 	public:
 		MPU6050(TwoWire *wire){
 			this->_wire = wire;
 		}
 
 		void begin(){
-
 			// Initialize the I2C-bus.
 			this->_wire->begin();
-			int error;
-			uint8_t c;
-
-			// default at power-up:
-			//    Gyro at 250 degrees second
-			//    Acceleration at 2g
-			//    Clock source at internal 8MHz
-			//    The device is in sleep mode.
 
 			// Clear the 'sleep' bit to start the sensor.
-			write_reg (MPU6050_PWR_MGMT_1, 0);
+			this->write (MPU6050_PWR_MGMT_1, 0, 1);
+
+			nh.advertise(imu_state);
+			imu_msg.data_length = 3;
+			Scheduler.startLoop(imuLoop);
 		}
-// --------------------------------------------------------
-// MPU6050_read
-//
-// This is a common function to read multiple bytes 
-// from an I2C device.
-//
-// It uses the boolean parameter for Wire.endTransMission()
-// to be able to hold or release the I2C-bus. 
-// This is implemented in Arduino 1.0.1.
-//
-// Only this function is used to read. 
-// There is no function for a single byte.
-//
-	int read(int start, uint8_t *buffer, int size){
-		int i, n, error;
-		this->_wire->beginTransmission(MPU6050_I2C_ADDRESS);
-		n = this->_wire->write(start);
-		if (n != 1) return (-10);
 
-		n = this->_wire->endTransmission(false);    // hold the I2C-bus
-		if (n != 0) return (n);
+		//read accelerometer data into x,y,z float buffer in Gs
+		void readAccel(float *buffer){
+			this->read (MPU6050_ACCEL_XOUT_H, (uint8_t *) &this->_accel_t, sizeof(this->_accel_t));
 
-		// Third parameter is true: relase I2C-bus after data is read.
-		this->_wire->requestFrom(MPU6050_I2C_ADDRESS, size, true);
-		i = 0;
-		while(this->_wire->available() && i<size) buffer[i++]=this->_wire->read();
+			// Swap all high and low bytes to low byte first.
+			uint8_t swap;
+			#define SWAP(x,y) swap = x; x = y; y = swap
 
-		if ( i != size) return (-11);
+			SWAP (this->_accel_t.reg.x_accel_h, this->_accel_t.reg.x_accel_l);
+			SWAP (this->_accel_t.reg.y_accel_h, this->_accel_t.reg.y_accel_l);
+			SWAP (this->_accel_t.reg.z_accel_h, this->_accel_t.reg.z_accel_l);
 
-		return (0);  // return : no error
-	}
+			for(int i = 0; i < 3; i++) buffer[i] = ((float) this->_accel_t.accel[i]) / 16384.0;
+		}
 
+		//read bytes from the accelerometer
+		void read(int start, uint8_t *buffer, int size){
+			this->_wire->beginTransmission(MPU6050_I2C_ADDRESS);
+			this->_wire->write(start);
+			this->_wire->endTransmission(false);    // hold the I2C-bus
+			this->_wire->requestFrom(MPU6050_I2C_ADDRESS, size, true);
+			int i = 0;
+			while(this->_wire->available() && i<size) buffer[i++]=this->_wire->read();
+		}
 
-	// --------------------------------------------------------
-	// MPU6050_write
-	//
-	// This is a common function to write multiple bytes to an I2C device.
-	//
-	// If only a single register is written,
-	// use the function MPU_6050_write_reg().
-	//
-	// Parameters:
-	//   start : Start address, use a define for the register
-	//   pData : A pointer to the data to write.
-	//   size  : The number of bytes to write.
-	//
-	// If only a single register is written, a pointer
-	// to the data has to be used, and the size is
-	// a single byte:
-	//   int data = 0;        // the data to write
-	//   MPU6050_write (MPU6050_PWR_MGMT_1, &c, 1);
-	int write(int start, const uint8_t *pData, int size)
-	{
-	  int n, error;
-
-	  this->_wire->beginTransmission(MPU6050_I2C_ADDRESS);
-	  n = this->_wire->write(start); // write the start address
-	  if (n != 1) return (-20);
-
-	  n = this->_wire->write(pData, size);  // write data bytes
-	  if (n != size) return (-21);
-
-	  error = this->_wire->endTransmission(true); // release the I2C-bus
-	  return error;
-	}
-
-	// --------------------------------------------------------
-	// write_reg
-	//
-	// An extra function to write a single register.
-	// It is just a wrapper around the MPU_6050_write()
-	// function, and it is only a convenient function
-	// to make it easier to write a single register.
-	int write_reg(int reg, uint8_t data){
-		return this->write(reg, &data, 1);
-	}
+		//write bytes to the accelerometer
+		void write(int start, const uint8_t *pData, int size)
+		{
+			this->_wire->beginTransmission(MPU6050_I2C_ADDRESS);
+			this->_wire->write(start); // write the start address
+			this->_wire->write(pData, size);  // write data bytes
+			this->_wire->endTransmission(true); // release the I2C-bus
+		}
 };
+
+
+MPU6050 IMU = MPU6050(&Wire);
+
+void imuLoop(){
+  IMU.readAccel((float *) &accel_f);
+
+  imu_msg.data = (float *) &accel_f;
+  imu_state.publish(&imu_msg);
+
+  //4 Hz update rate
+  delay(250);
+}
+
 #endif
