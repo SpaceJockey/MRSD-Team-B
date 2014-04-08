0d28b7671c7cdf9e1bdccce173eb3a142f9e82f8
Unchapped <nate@nathanielchapman.com>
2014-04-08 19:12:11 -0400

    added heartbeat monitor to prototype code

--- Arduino/prototypes/PWMDigitalCalibrate/PWMDigitalCalibrate.ino
+++ Arduino/prototypes/PWMDigitalCalibrate/PWMDigitalCalibrate.ino
@@ -1,19 +1,20 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMID  1900
+#define SERVOMIN_US  900
+#define SERVOMID_US  1500
+#define SERVOMAX_US  2100
 
 #define FEEDBACK_CHAN 15
 #define FEEDBACK_PIN 22
 
+//for testing
 #define SERVO_CHAN 1
 
-//Need to be unsigned long on AVR devices
-//static volatile unsigned int startus = 0;
 static volatile unsigned int pulseus = 0;
+
 
 void readPulse(){
 	unsigned int currus = micros();
@@ -25,42 +26,64 @@ void readPulse(){
 	}
 }
 
+inline void attachInt(){attachInterrupt(FEEDBACK_PIN, readPulse, CHANGE);}
+inline void detachInt(){detachInterrupt(FEEDBACK_PIN);}
+
+//last calibrated values
+int servomin = 1200;
+int servomax = 2700;
+
+int findTarget(int target, int clue=SERVOMID_US){
+	int error = 1;
+	int pulselen = clue;
+	while (error != 0){
+		pulselen = pulselen + error;
+		pwm.setPWM(FEEDBACK_CHAN, 0, pulselen); //should take 100us in fast mode
+		delay(6); //should give enough time for the pulse width to change and 2 pulses to hit;
+		error = target - pulseus;
+	}
+	return pulselen;
+}
+
 void setup() {
-	int servomin = 0;
-	int servomax = 9999;
+	Serial.begin(9600);
+	Serial.println("Digital PWM calibration sketch");
 
 	pwm.begin();
 	TWI_ConfigureMaster(WIRE_INTERFACE, 400000, VARIANT_MCK); //I2C Fast mode
 	pwm.setPWMFreq(300);  // Digital servos run at 300 Hz updates
-	attachInterrupt(FEEDBACK_PIN, readPulse, CHANGE);
-	pwm.setPWM(FEEDBACK_CHAN, 0, SERVOMID);
-	servomin = findTarget(900);
-	servomax = findTarget(2100);
-	detachInterrupt(FEEDBACK_PIN);
+	pwm.setPWM(FEEDBACK_CHAN, 0, SERVOMID_US);
+	attachInt();
+	servomin = findTarget(SERVOMIN_US, servomin);
+	servomax = findTarget(SERVOMAX_US, servomin);
+	detachInt();
 
-	
-	Serial.begin(9600);
-	Serial.println("Digital PWM calibration sketch");
 	Serial.print("Minimum (900 us): ");
 	Serial.println(servomin);
 	Serial.print("Maximum (2100 us): ");
 	Serial.println(servomax);
 }
 
-int findTarget(int target){
-	//int target = tgt;
-	int error = 1;
-	int pulselen = SERVOMID;
-	while (error != 0){
-		pulselen = pulselen + ((error *3) / 4);
-		pwm.setPWM(FEEDBACK_CHAN, 0, pulselen); //should take 100us in fast mode
-		delay(10); //should give enough time for the pulse width to change and 3 pulses to hit
-		//while(!gotPulse) delay(1);
-		error = target - pulseus;
-	}
-	return pulselen;
-}
-
-
+int servoval = servomax;
+//motor monitoring loop
 void loop() {
+	servoval = (servomax - servoval) + servomin;
+	pwm.setPWM(SERVO_CHAN, 0, servoval);
+
+	Serial.print("Checking calibration...");
+	pulseus = 0;
+	attachInt();
+	pwm.setPWM(FEEDBACK_CHAN, 0, servomin);
+	delay(6);
+	int error = SERVOMIN_US - pulseus;
+	pwm.setPWM(FEEDBACK_CHAN, 0, servomax);
+	delay(6);
+	error += SERVOMAX_US - pulseus;
+	detachInt();
+	if (abs(error) > 4){
+		Serial.println("WARNING");
+	}else{
+		Serial.println("OK");
+	}
+	delay(1000);
 }
