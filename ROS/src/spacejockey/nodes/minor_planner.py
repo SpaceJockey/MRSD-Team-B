8b824397095d092d433f752a6b17930a7046a81a
Unchapped <nate@nathanielchapman.com>
2014-04-12 18:34:23 -0400

    added pause minor action

--- ROS/src/spacejockey/nodes/minor_planner.py
+++ ROS/src/spacejockey/nodes/minor_planner.py
@@ -2,155 +2,137 @@
 import roslib
 roslib.load_manifest('spacejockey')
 import rospy
-from collections import OrderedDict, deque
-from Queue import Queue
+from collections import deque, namedtuple
 import spacejockey
-from spacejockey.msg import MajorPlanAction, MinorPlanAction
+from spacejockey.kinematics import IK
+from spacejockey.msg import MajorPlanAction
 import tf
 from sensor_msgs.msg import JointState
 import math
+import argparse
 
-#floating point error compensation
-ferr = 0.0001
+
 
 #TODO: load these from URDF
-joint_names = [
-  'center_swivel',
-  'fore_base_pitch',
-  'aft_base_pitch',
-  'front_pitch',
-  'rear_pitch',
-  'fore_extend',
-  'aft_extend',
-  'front_attach',
-  'center_attach',
-  'rear_attach']
 node_names =  ['front_foot', 'center_foot', 'rear_foot']
-movequeue = deque()
-actionqueue = Queue()
+minorqueue = deque()
+actionqueue = deque()
 
-#movequeue.append(IK('...'))
-#rospy.loginfo(msg)
-
-#This is a hackackular quick and dirty implementation, needs to be fixed long term
-#takes X, Y, Z tuples, returns a set of joint angles
-def IK(front,center,rear):
-  #initialize joint positions...
-  positions = OrderedDict.fromkeys(joint_names, 0.0)
-  #step one, compute detach states
-  positions['front_attach'] = 0.0 if front[0] < ferr else 0.006 
-  positions['center_attach'] = 0.0 if center[0] < ferr else 0.006 
-  positions['rear_attach'] = 0.0 if rear[0] < ferr else 0.006 
-
-  f_xy_dist = math.sqrt((front[0] - center[0])**2 + (front[1] - center[1])**2) - .07054 
-  f_z_dist = center[2] - front[2] - .0194
-
-  r_xy_dist = math.sqrt((center[0] - rear[0])**2 + (center[1] - rear[1])**2) - .07054
-  r_z_dist = center[2] - rear[2] - .0194
-
-  front_theta = math.atan2(front[1] - center[1], front[0] - center[0])
-  rear_theta = math.atan2(center[1] - rear[1], center[0] - rear[0])
-  positions['center_swivel'] = math.atan2(math.sin(front_theta-rear_theta), math.cos(front_theta-rear_theta))
-  positions['fore_extend'] = math.sqrt(f_xy_dist**2 + f_z_dist**2)
-  positions['aft_extend'] = math.sqrt(r_xy_dist**2 + r_z_dist**2)
-
-  ftheta = math.atan2(f_z_dist, f_xy_dist)
-  rtheta = math.atan2(r_z_dist, r_xy_dist)
-
-  positions['fore_base_pitch'] = ftheta
-  positions['front_pitch'] = -ftheta
-  positions['aft_base_pitch'] = rtheta
-  positions['rear_pitch'] = -rtheta
-  return positions
+MinorAction = namedtuple('MinorAction', 'name pos detach')
+PauseAction = namedtuple('PauseAction', 'name duration')
+float_error = .0001
 
 class MinorPlanner:
-  def __init__(self):
+  def __init__(self, rate = 10):
+    self.Hz = rate
     self.config = spacejockey.config("/planner")
-    self.jointPub = rospy.Publisher('joint_minor', JointState)
+
+    self.joint_tgt = dict()
+    self.joints = dict()
+    self.isPaused = False
+
+    self.jointPub = rospy.Publisher('joint_states_planned', JointState)    
     rospy.Subscriber('major_actions', MajorPlanAction, self.handle_major_action)
+    rospy.Subscriber('joint_states', JointState, self.handle_joint_states)
     rospy.init_node('minor_planner', anonymous=True)
+
     self.tf = tf.TransformListener()
     self.tfCast = tf.TransformBroadcaster()
 
-    self.lastTf = [((self.config.extend.min,0,0), tf.transformations.quaternion_from_euler(0, 0, -1.57)),
-                   ((0,0,0), tf.transformations.quaternion_from_euler(0, 0, -1.57)),
-                   ((-self.config.extend.min,0,0), tf.transformations.quaternion_from_euler(0, 0, -1.57))]
+    self.lastTf = [((self.config.extend.min,0,0), tf.transformations.quaternion_from_euler(0, 0, 0)),
+                   ((0,0,0), tf.transformations.quaternion_from_euler(0, 0, 0)),
+                   ((-self.config.extend.min,0,0), tf.transformations.quaternion_from_euler(0, 0, 0))]
 
+  def handle_joint_states(self, msg):
+    for i in range(len(msg.name)):
+      self.joints[msg.name[i]] = msg.position[i]
 
+  def is_at_joint_tgt(self):
+    for j in self.joint_tgt.keys():
+      if abs(self.joints[j] - self.joint_tgt[j]) > float_error:
+        return False
+    return True
 
+  def update_joints(self):
+    joints = self.joints #TODO: add joint velocities to self.joints
+    j_msg = JointState()
+    j_msg.name = joints.keys()
+    j_msg.position = joints.values() 
+    self.jointPub.publish(joint_msg)
+    return True
 
+  def unpause(self, event):
+    self.isPaused = False
 
+  def execute_minor_action(self, act):
+    if act.name == 'pause':
+      self.isPaused = True
+      rospy.Timer(rospy.Duration(act.duration), self.unpause, True) #oneshot timer
+    #TODO: pull tfs, execute joint angles
+    return
 
   def execute_move_action(self, msg):
+    node = node_names.index(msg.node_name)
     try:
-      (ftrans, frot) = self.lastTf[0] #self.tf.lookupTransform('/world', '/front_foot', rospy.Time(0))
-      (ctrans, crot) = self.lastTf[1] #self.tf.lookupTransform('/world', '/center_foot', rospy.Time(0))
-      (rtrans, rrot) = self.lastTf[2] #self.tf.lookupTransform('/world', '/rear_foot', rospy.Time(0))
+      (loc, rot) = self.tf.lookupTransform('/world', msg.node_name, rospy.Time(0)) #try to get tf from world frame...
+    except:
+      (loc, rot) = self.lastTf[node] #fall back to internal model
+    loc = list(loc)
+    loc[2] = self.config.move.detachHeight #detach
+    minorqueue.append(MinorAction(msg.node_name, tuple(loc), True))
 
-      #authoritatively repuplish nearest neighbor transform to hold robot position to world
-      #TODO: fix this...
-      #if(msg.node_name != 'center_foot'):
-      self.tfCast.sendTransform(ctrans, crot, rospy.Time.now(), "/center_foot", "/world")
-      #else:
-      #  self.tfCast.sendTransform(rtrans, rrot, rospy.Time.now(), "/rear_foot", "/world")
+    loc[2] = self.config.move.clearHeight #apex
+    loc[0] = (loc[0] + msg.x) / 2.0
+    loc[1] = (loc[1] + msg.y) / 2.0
+    minorqueue.append(MinorAction(msg.node_name, tuple(loc), False))
 
-      node = node_names.index(msg.node_name)
-      robot = [list(ftrans), list(ctrans), list(rtrans)]
+    loc[2] = self.config.move.detachHeight #pre-landing
+    loc[0] = msg.x
+    loc[1] = msg.y
+    minorqueue.append(MinorAction(msg.node_name, tuple(loc), False))
 
-      robot[node][2] = self.config.move.detachHeight #detach
-      movequeue.append(IK(*tuple(robot)))
-
-      robot[node][2] = self.config.move.clearHeight #apex
-      robot[node][0] = (robot[node][0] + msg.x) / 2
-      robot[node][1] = (robot[node][1] + msg.y) / 2
-      movequeue.append(IK(*tuple(robot)))
-
-      robot[node][2] = self.config.move.detachHeight #pre-landing
-      robot[node][0] = msg.x
-      robot[node][1] = msg.y
-      movequeue.append(IK(*tuple(robot)))
-
-      robot[node][2] = 0.0 #attach
-      movequeue.append(IK(*tuple(robot)))
-      self.lastTf[node_names.index(msg.node_name)] = ((msg.x, msg.y, 0.0), tf.transformations.quaternion_from_euler(0, 0, msg.theta))
-
-
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-      rospy.logerr('Failed to get transform: from /world to /' + msg.node_name)
-      #actionqueue.appendleft(msg
-    return
+    loc[2] = 0.0 #attach
+    minorqueue.append(MinorAction(msg.node_name, tuple(loc), False))
+    self.lastTf[node] = ((msg.x, msg.y, 0.0), tf.transformations.quaternion_from_euler(0, 0, msg.theta))
 
   def execute_view_action(self, msg):
+    #TODO: make this a thing!
+
+    #queue up a pause action
+    print('view action')
+    minorqueue.append(PauseAction('pause', 3))
     return
 
-  #handle this thread safely
   def handle_major_action(self, msg):
-    actionqueue.put(msg)
+    actionqueue.append(msg)
 
 
   def loop(self):
+    rate = rospy.Rate(self.Hz)
     while not rospy.is_shutdown():
-      if len(movequeue) > 0:
-        next = movequeue.popleft()  #next is an ordered dict...
-        joint_msg = JointState()
-        joint_msg.name = next.keys()
-        joint_msg.position = next.values() 
-        self.jointPub.publish(joint_msg)
-        #TODO: delay long enough for slow joints to settle
-        #TODO: publish world frame transforms...
-      else:
-        if not actionqueue.empty():
-          msg = actionqueue.get()
-          if(msg.action_type == MajorPlanAction.STEP):
-            self.execute_move_action(msg)
-          else:
-            self.execute_view_action(msg)
-      rospy.sleep(1)
+      if self.isPaused:
+        pass #do nothing...
+      elif not self.is_at_joint_tgt():
+        self.update_joints()
+      elif minorqueue:
+        print('minor action')
+        next = minorqueue.popleft() 
+        self.execute_minor_action(next)
+      elif actionqueue:
+        msg = actionqueue.popleft()
+        print('major action')
+        if(msg.action_type == MajorPlanAction.STEP):
+          self.execute_move_action(msg)
+        else:
+          self.execute_view_action(msg)
+      rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        planner = MinorPlanner()
-        #Wee bit hacky... may need moved somehwere...
-        #planner.tfCast.sendTransform((0,0,0), (0,0,0,1), rospy.Time.now(), "/center_foot", "/world")
-        planner.loop()
-    except rospy.ROSInterruptException: pass
+  parser = argparse.ArgumentParser(prog='minor_planner.py', description="this node breaks down moves into their constituent parts and executes them.")
+  #parser.add_argument('-f', '--root_frame', default='world', help='the root frame_id to tie transforms to')
+  parser.add_argument('-r', '--rate', type=int, default=10, help='motor control output rate (in Hz)')
+  args, unknown = parser.parse_known_args()
+  try:
+    planner = MinorPlanner(args.rate)
+    planner.loop()
+  except rospy.ROSInterruptException: pass
