26e6eee19d92d715b780a926724ce056cf54bee3
ardyadipta <ardyadipta@gmail.com>
2014-04-15 11:18:41 -0400

    Added spacecam, works with AR Track

--- ROS/src/spacecam/scripts/spacecam.py
+++ ROS/src/spacecam/scripts/spacecam.py
@@ -23,11 +23,11 @@ class SpaceJockeyCam(object):
         self.bytes=''
         self.width = 640
         self.height = 480
-        self.frame_id = 'spacecam'
-        self.image_pub = rospy.Publisher("spacecam/image_raw", Image)
-        self.cinfo = camera_info_manager.CameraInfoManager(cname = 'spacecam', url = 'PACKAGE://spacecam/config/calibration.yaml')
+        self.frame_id = 'camera'
+        self.image_pub = rospy.Publisher("camera/image_raw", Image)
+        self.cinfo = camera_info_manager.CameraInfoManager(cname = 'camera', url = 'PACKAGE://spacecam/config/calibration.yaml')
         self.cinfo.loadCameraInfo()         # required before getCameraInfo()
-        self.caminfo_pub = rospy.Publisher("spacecam/camera_info", CameraInfo)
+        self.caminfo_pub = rospy.Publisher("camera/camera_info", CameraInfo)
         self.bridge = CvBridge()
 
     def publishCameraInfoMsg(self):
@@ -38,6 +38,14 @@ class SpaceJockeyCam(object):
         cimsg.width = self.width
         cimsg.height = self.height
         self.caminfo_pub.publish(cimsg)
+
+    def publishMsg(self):
+        '''Publish jpeg image as a ROS message'''
+        self.msg = Image()
+        self.msg.header.stamp = rospy.Time.now()
+        self.msg.header.frame_id = self.frame_id
+        self.msg.data = self.img
+        self.axis.pub.publish(self.msg)
 
 def main():
     parser = argparse.ArgumentParser(prog='spacecam.py', description='reads a given url string and dumps it to a ros_image topic')
@@ -56,14 +64,16 @@ def main():
             jpg = spacecam.bytes[a:b+2]
             spacecam.bytes= spacecam.bytes[b+2:]
             i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR)
-            image_message = spacecam.bridge.cv2_to_imgmsg(i, "passthrough")
+            image_message = spacecam.bridge.cv2_to_imgmsg(i, "bgr8")
+            image_message.header.stamp = rospy.Time.now()
+            image_message.header.frame_id = spacecam.frame_id
             spacecam.image_pub.publish(image_message)
+            spacecam.publishCameraInfoMsg()
             print "height: ", image_message.height
             print "width: ", image_message.width
             print "encoding: ", image_message.encoding
             print "step: ", image_message.step
-            spacecam.publishCameraInfoMsg()
-
+            print "header: ", image_message.header
             if args.gui:
                 cv2.imshow('Space Jockey Publisher Cam',i)
             if cv2.waitKey(1) ==27: # wait until ESC key is pressed in the GUI window to stop it
